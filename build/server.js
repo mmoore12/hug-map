e9f3e18413eed7f08e197e3722b65c1c62e57317
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-05-23 12:33:54 +0300

    Fix url

--- build/server.js
+++ build/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
-  res.sendfile('./dist/index.html');
+  res.sendfile('dist/index.html');
 });
 
 app.listen(port, function () {
